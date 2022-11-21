e2802474a4190076b172b9ef17ffe06edafdecb1
Ivan Samsonov <suckertwist@gmail.com>
2022-11-21 10:02:32 +0300

    Выполнение задания 6го спринта

---
+++ src/test/java/com/example/FelineTest.java
@@ -0,0 +1,34 @@
+package com.example;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class FelineTest {
+
+    @Test
+    public void felineEatMeatTest() throws Exception {
+        Feline feline = new Feline();
+        List<String> actualFoodList = feline.eatMeat();
+        List<String> expectedList = Arrays.asList("Животные", "Птицы", "Рыба");
+        assertEquals(expectedList, actualFoodList);
+    }
+
+    @Test
+    public void felineGetFamilyTest() {
+        Feline feline = new Feline();
+        String actualFamaly = feline.getFamily();
+        assertEquals("Кошачьи", actualFamaly);
+    }
+
+    @Test
+    public void felineKittensTest() {
+        Feline feline = new Feline();
+        int actualKittens = feline.getKittens();
+        assertEquals(1, actualKittens);
+    }
+}
